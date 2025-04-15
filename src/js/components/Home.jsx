634343d2d1948ca72c99fad49b1708b9d49aa6d7
JorgeArevalo-hub <jorgearevalomoran@gmail.com>
2025-04-15 22:01:24 +0000

    f: get ready, pending modify/delete

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,50 @@
 import React from "react";
+import { useEffect,useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	
+	const [data, setData] = useState("");
+	const [id, setId] = useState("");
+	const [store, setStore] = useState([]);
+
+	const getDataFunction = async () => {
+		try{
+			const getFectch = await fetch("https://playground.4geeks.com/todo/users");
+			const getData = await getFectch.json();
+				setStore(getData.users)
+
+		}catch(error){
+			console.log(error)
+		}
+	}
+
+
+
+	useEffect(() => {
+		getDataFunction();
+	}, []);
+
+
+	return (
+		<div>
+			<h1 className="text-center p-3">My TodoList with React & Fetch</h1>
+			<table className="justificado">
+				<thead>
+					<tr>
+						<th>id</th>
+						<th>name</th>
+						<th>actions</th>
+					</tr>
+				
+				</thead>
+				<tbody>
+					{store.map(el => <tr key={el.id}><td>{el.id}</td><td>{el.name}</td><td><button>Modificar</button><button>Borrar</button></td></tr>)}
+					<tr><td></td><td><input type="text" placeholder="your name"></input></td><td><button>Crear</button></td></tr>
+				</tbody>
+			</table>
 		</div>
 	);
 };
